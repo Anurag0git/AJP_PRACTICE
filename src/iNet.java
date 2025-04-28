52e31ac44f1acec2b1959020e865bccebd47c49a
Anurag <130895173+Anurag0git@users.noreply.github.com>
2025-04-29 03:08:25 +0530

    Update iNet.java

--- src/iNet.java
+++ src/iNet.java
@@ -4,7 +4,7 @@ import java.net.*;
 public class InetDemo {
     public static void main(String[] args) {
         try {
-            InetAddress ip = InetAddress.getByName("www.pcceor.com");
+            InetAddress ip = InetAddress.getByName("www.pccoer.com");
             System.out.println("Host Name: " + ip.getHostName());
             System.out.println("IP Address: " + ip.getHostAddress());
         } catch (Exception e) {
