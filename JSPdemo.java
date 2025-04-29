c77645cf49aa7dca9f12ebf23e43dff5cb1afe33
Anurag <130895173+Anurag0git@users.noreply.github.com>
2025-04-29 08:51:52 +0530

    Create JSPdemo.java

---
+++ JSPdemo.java
@@ -0,0 +1,18 @@
+// Steps to create JSP page
+// https://www.javatpoint.com/creating-jsp-in-eclipse-ide
+// Create dynamic web project
+// Create 1 jsp file in it
+// First.jsp
+
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+   <h1>Welcome to PCCOER</h1>
+</body>
+</html>
